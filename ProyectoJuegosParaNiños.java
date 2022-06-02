5be12964bd1395224039c08b2e276ad61ff95db8
Reynahernandez <106767946+Reynahernandez@users.noreply.github.com>
2022-06-02 11:38:06 -0600

    Add files via upload

---
+++ ProyectoJuegosParaNiños.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto.juegos.para.niños;
+
+/**
+ *
+ * @author Reyna
+ */
+public class ProyectoJuegosParaNiños {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
